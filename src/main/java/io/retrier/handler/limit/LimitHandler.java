e89b13c42cd377ac4cda9bf369c56878bee9a027
Ravi Chaturvedi <ravichaturvedi1@gmail.com>
2017-07-06 09:21:30 +0530

    Using AUTHORS in copyright header.

--- src/main/java/io/retrier/handler/limit/LimitHandler.java
+++ src/main/java/io/retrier/handler/limit/LimitHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Ravi Chaturvedi
+ * Copyright 2017 The Retrier AUTHORS.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
